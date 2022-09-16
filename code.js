4b8126b2651cab797ce6130d136d658c56267428
Maikel Karam <maikel.m.karam@gmail.com>
2022-09-15 22:09:12 -0500

    unit tests for edge cases

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 let helloWorld = function() {
     return "Hello, World!";
 }
-
+//Exercises 1-9
 function sayHello(input){
     if(typeof input !== "string" || input === ""){
         return "Hello, World!"
